9f3b788e77fed6c582369fdbbe046594333cd6af
Tao Peng <pt@taopeng.me>
2011-11-02 02:22:53 +0100

    move class `Iterator' to pt-iterator.el; remove variable `pt-font-list'. use `pt-fonts' instead.

--- examples/pt-find-fonts.el
+++ examples/pt-find-fonts.el
@@ -1,59 +1,5 @@
 (require 'cl)
-(require 'class)
-
-(defvar pt-font-list '("Courier New-14"
-                       "Meslo LG S DZ-11"
-                       "Andale Mono-14"
-                       "Consolas-14"
-                       "Courier-14"))
-
-(class Iterator (Object)
-       (defun init (self set)
-         (self. set (remove-duplicates set))
-         (self. pos 0))
-
-       (defun length (self)
-         (length (self. set)))
-
-       (defun next (self &optional n)
-         (when (> (self. length) 1)
-           (setq n (or n 1))
-           (self. pos (mod (+ n (self. pos))
-                           (self. length)))
-           (self. current)))
-
-       (defun current (self)
-         (nth (self. pos) (self. set)))
-
-       (defun shuffle (self)
-         (when (> (self. length) 1)
-           (let ((n (1+ (random (self. length)))))
-             (while (= n (self. length))
-               (setq n (1+ (random (self. length)))))
-             (self. next n))))
-
-       (defun reset (self)
-         (self. pos 0))
-
-       (defun push (self elm)
-         (let ((set (self. set))
-               (pos (self. pos)))
-           (if (= 0 pos)
-               (self. set (cons elm set))
-             (setcdr (nthcdr (1- pos) set)
-                     (cons elm (nthcdr pos set))))
-           elm))
-
-       (defun pop (self)
-         (let ((set (self. set))
-               (pos (self. pos))
-               (top (self. current)))
-           (if (= 0 pos)
-               (self. set (cdr set))
-             (setcdr (nthcdr (1- pos) set)
-                     (nthcdr (1+ pos) set)))
-           (self. next)
-           top)))
+(require 'pt-iterator)
 
 (class Fonts (Iterator)
        (staticmethod
@@ -73,8 +19,7 @@
              (set-frame-font font))
            font)))
 
-(setq pt-fonts
-      (Fonts pt-font-list))
+(setq pt-fonts (Fonts nil))
 
 (defun pt-font-shuffle ()
   (interactive)
